a60ae68eea8e11fde8c3731bef05e8d3d716f80b
Vimal Saraswat <saraswatvimal1@gmail.com>
2026-07-18 22:58:19 +0530

    feat: start axum server on port 3000

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use api::router;
+
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+
+    axum::serve(listener, router::router()).await.unwrap();
 }
