bedd0a7ee429b0ba26d52c190b2cba9741430357
Vimal Saraswat <saraswatvimal1@gmail.com>
2026-08-12 21:48:00 +0530

    feat: add put and delete methods to api client

--- apps/web/app/services/api.ts
+++ apps/web/app/services/api.ts
@@ -1,8 +1,11 @@
 export interface ApiClient {
   get<T>(path: string): Promise<T>
-  post(path: string): Promise<unknown>
+  post<T, B = unknown>(path: string, body?: B): Promise<T>
+  put<T, B = unknown>(path: string, body: B): Promise<T>
+  delete<T>(path: string): Promise<T>
 }
 
+// TODO: make more typesafe
 export function createApiClient(baseURL: string): ApiClient {
   return {
     get<T>(path: string) {
@@ -12,9 +15,27 @@ export function createApiClient(baseURL: string): ApiClient {
       })
     },
 
-    post(path: string) {
-      return $fetch<unknown>(path, {
+    post<T, B extends BodyInit | Record<string, any> | null | undefined>(path: string, body?: B) {
+      return $fetch<T>(path, {
         method: 'POST',
+        baseURL,
+        credentials: 'include',
+        body,
+      })
+    },
+
+    put<T, B extends BodyInit | Record<string, any> | null | undefined>(path: string, body: B) {
+      return $fetch<T>(path, {
+        method: 'PUT',
+        baseURL,
+        credentials: 'include',
+        body,
+      })
+    },
+
+    delete<T>(path: string) {
+      return $fetch<T>(path, {
+        method: 'DELETE',
         baseURL,
         credentials: 'include',
       })
